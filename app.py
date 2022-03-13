34f7dbc155da569f49ff991c08551ba5df5a1177
LeslieR0SS <laranibarpozo@cifpfbmoll.eu>
2022-03-13 03:02:41 +0100

    fix: :zap: debugger is active

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+from distutils.log import debug
 from flask import Flask 
 app = Flask(__name__)
 
 @app.route('/') 
-def index():
+def hello_world():
     return '<h1>Hello World!</h1>'
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0',8080, debug= True)
