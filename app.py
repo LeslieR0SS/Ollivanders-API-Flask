5a72ccd47ea00d00cb9b90e04013d1be86871749
LeslieR0SS <laranibarpozo@cifpfbmoll.eu>
2022-03-14 00:24:58 +0100

    refactor: :construction: in progress connection to db

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from distutils.log import debug
-from flask import Flask 
+from flask import Flask, g
 from flask_restful import Api
 
 
@@ -28,22 +28,22 @@ def create_app():
     return app
 
 
-# #Rutas estáticas
-# @app.route('/') 
-# def hello_world():
-#     return '<h1>Hello World!</h1>'
+# # #Rutas estáticas
+# # @app.route('/') 
+# # def hello_world():
+# #     return '<h1>Hello World!</h1>'
 
-# #Rutas dinánimas:
+# # #Rutas dinánimas:
 
-# @app.route("/hello/<string:nombre>")
-# @app.route("/hello/<string:nombre>/<int:edad>")
-# def hola(nombre=None,edad=None):
-#     if nombre and edad:
-#         return 'Hola, {0} tienes {1} años.'.format(nombre,edad)
-#     elif nombre:
-#         return 'Hola, {0}'.format(nombre)
-#     else:
-#         return 'Hola mundo'
+# # @app.route("/hello/<string:nombre>")
+# # @app.route("/hello/<string:nombre>/<int:edad>")
+# # def hola(nombre=None,edad=None):
+# #     if nombre and edad:
+# #         return 'Hola, {0} tienes {1} años.'.format(nombre,edad)
+# #     elif nombre:
+# #         return 'Hola, {0}'.format(nombre)
+# #     else:
+# #         return 'Hola mundo'
 
 if __name__ == '__main__':
     app = create_app()
--- repository/db_connection.py
+++ repository/db_connection.py
@@ -1,2 +1,12 @@
-from mongoengine import *
-from repository import db_uri
+
+
+# from mongoengine import *
+# from repository.db_uri import db_uri
+# from mongoengine import connect
+# import certifi
+
+
+# def db_connection():
+#     ca = certifi.where()
+#     conexion = connect(host=DB_URI, tlsCAFile=ca)
+#     return conexion["ollivanders"]["ollivanders"]
