ab64233642cecc3b33f6a2cfe3d0ca9a70ab88ac
LeslieR0SS <laranibarpozo@cifpfbmoll.eu>
2022-03-18 06:21:07 +0100

    feat: :card_file_box: items on stock

---
+++ repository/items.py
@@ -0,0 +1,49 @@
+items = [
+    {
+        "name": "PEPITOO", 
+        "sell_in": 10, 
+        "quality": 20
+        },
+    
+    {
+        "name": "AgedBrie", 
+        "sell_in": 2, 
+        "quality": 0},
+    {
+        "name": "NormalItem",
+        "sell_in": 5,
+        "quality": 7},
+    
+    {
+        "name": "Sulfuras",
+        "sell_in": 0, 
+        "quality": 80
+        },
+    
+    {
+        "name": "Mariposa",
+        "sell_in": -1, 
+        "quality": 80
+        },
+    
+    {
+        "name": "Backstage",
+        "sell_in": 15, 
+        "quality": 20},
+    
+    {
+        "name": "Scorpion",
+        "sell_in": 10,
+        "quality": 49
+        },
+    
+    {
+        "name": "Backstage", 
+        "sell_in": 10,
+        "quality": 30},
+    
+    {"name": "ConjuredItem",
+    "sell_in": 3, 
+    "quality": 6
+    },
+]
